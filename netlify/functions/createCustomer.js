f9d8748f61b45ed6c9c53b0e817f5c9da6fe7936
amagnien <amagnien@gmail.com>
2024-10-10 11:04:41 +0400

    Delete netlify/functions directory

--- netlify/functions/createCustomer.js
+++
@@ -1,9 +0,0 @@
-exports.handler = async (event, context) => {
-    const data = JSON.parse(event.body);
-    // Store customer data in a database or other storage
-    // ...
-    return {
-        statusCode: 200,
-        body: JSON.stringify({ message: 'Customer created successfully' })
-    };
-};
--- netlify/functions/deleteCustomers.js
+++
@@ -1,8 +0,0 @@
-exports.handler = async (event, context) => {
-    // Delete all customer data from storage
-    // ...
-    return {
-        statusCode: 200,
-        body: JSON.stringify({ message: 'All customers deleted' })
-    };
-};
